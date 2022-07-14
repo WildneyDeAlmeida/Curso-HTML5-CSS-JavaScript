200c3640191aee9c450922003d25654cb90f5ea9
Wildney de Almeida Junior <wildneyjunior@gmail.com>
2022-07-14 12:47:08 -0300

    Integrando JS dentro do nosso index.html

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,6 @@
+function alteraIcone(foto) {
+    document.getElementById("icone").src="" + foto + "";
+}  
+function restauraIcone() {
+    document.getElementById("icone").src="_imagens/glass-oculos-preto-peq.png";
+} 
